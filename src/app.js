575a38922db6b15136e6168dc235e62d372e3b2d
Suman Biswas <suman.lalbagh93@gmail.com>
2022-08-24 01:53:23 +0530

    static website serve by express using express.static

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
+const staticPath = path.join(__dirname, '../public')
 
-app.get('/', (req, res) => {
-    res.send("Hello world")
-})
+app.use(express.static(staticPath));
+
+
 
 app.get('/about', (req, res) => {
     res.send("Hello world about")
