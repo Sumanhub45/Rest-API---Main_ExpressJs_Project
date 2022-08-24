f59fa7a249e564da59ac0eef0cd9fe45ce840009
Suman Biswas <suman.lalbagh93@gmail.com>
2022-08-24 12:08:18 +0530

    dynamic page has added by view engine

--- src/app.js
+++ src/app.js
@@ -2,11 +2,28 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8000;
+
 const staticPath = path.join(__dirname, '../public')
 
-app.use(express.static(staticPath));
+// // to use static website in express
+// app.use(express.static(staticPath));
+
+// // to set the view engine
+app.set("view engine", "hbs");
 
 
+
+
+// // to render dynamic page
+app.get('/', (req, res) => {
+    res.render('index',{ 
+        name:"home"
+    })
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
 
 app.get('/about', (req, res) => {
     res.send("Hello world about")
