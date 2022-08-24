f07f3d91ecc4c838c29d92ead4bb8a176dbe11a6
Suman Biswas <suman.lalbagh93@gmail.com>
2022-08-24 12:42:47 +0530

    dynamic page and partials has been added

--- src/app.js
+++ src/app.js
@@ -1,23 +1,33 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const hbs = require('hbs');
 const PORT = process.env.PORT || 8000;
 
-const staticPath = path.join(__dirname, '../public')
+const staticPath = path.join(__dirname, '../public');
+const templatePath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 // // to use static website in express
 // app.use(express.static(staticPath));
 
 // // to set the view engine
 app.set("view engine", "hbs");
-
-
+app.set("views", templatePath);
+hbs.registerPartials(partialPath);
 
 
 // // to render dynamic page
 app.get('/', (req, res) => {
     res.render('index',{ 
         name:"home"
+    })
+})
+
+// // to render dynamic page
+app.get('/about', (req, res) => {
+    res.render('about',{ 
+        name:"about"
     })
 })
 
